daaad5bc5ffcb2a80007bfb9a081c2cad7413e59
ludawei <dawei_lu@126.com>
2015-07-22 18:16:36 +0800

    Initial Commit

---
+++ WLVedioPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WLVedioPlayerDemo
+//
+//  Created by 卢大维 on 15/7/22.
+//  Copyright (c) 2015年 weather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WLVedioPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLVedioPlayerDemo
+//
+//  Created by 卢大维 on 15/7/22.
+//  Copyright (c) 2015年 weather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
